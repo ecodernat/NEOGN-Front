5855f6dd8a4e0f9239dbbbdbc4b9fc80a4a4c6d7
Alan <alanjesuseliasruiz@gmail.com>
2023-10-15 13:46:26 -0300

    testing deploy

--- src/redux/actions/getProducts.js
+++ src/redux/actions/getProducts.js
@@ -4,7 +4,9 @@ import { getProducts } from "../slices/productsSlice";
 const fetchProducts = () => {
   return async function (dispatch) {
     try {
-      const json = await axios.get("/api/products");
+      const json = await axios.get(
+        "https://neogn-back.up.railway.app/api/products"
+      );
       const products = json.data;
       return dispatch(getProducts(products));
     } catch (error) {
