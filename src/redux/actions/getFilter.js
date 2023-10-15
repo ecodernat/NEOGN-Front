9a63f00cb631e284f58ffc7daaf45cb466b1d42f
Alan <alanjesuseliasruiz@gmail.com>
2023-10-15 14:02:10 -0300

    deploy

--- src/redux/actions/getFilter.js
+++ src/redux/actions/getFilter.js
@@ -4,12 +4,9 @@ import { setFilter } from "../slices/filterSlice";
 const getFilter = (filterParams) => {
   return async function (dispatch) {
     try {
-      const response = await axios.get(
-        `https://neogn-back.up.railway.app/api/products/filter`,
-        {
-          params: filterParams,
-        }
-      );
+      const response = await axios.get(`/api/products/filter`, {
+        params: filterParams,
+      });
 
       dispatch(setFilter(response.data));
     } catch (error) {
