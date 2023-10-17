a674b83c7782cf9c7f89d4469a2f10e3cd4f608c
maxianguita <maxi.7anguita@gmail.com>
2023-10-17 20:13:03 -0300

    cambios nuevos

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -4,6 +4,7 @@ import Sidebar from "./Sidebar";
 import ActiveHeartImage from "../../utils/images/AppbarIcons/Neogn.jpeg";
 import Banner from "./Banner";
 
+
 const TopBar = () => {
   // con este estado cuando se expanda el search se oculta todo
   const [isSearchFocused, setIsSearchFocused] = useState(false);
@@ -17,16 +18,16 @@ const TopBar = () => {
   return (
     <div> 
       <Banner /> 
-    <div className="w-full h-auto flex items-center justify-between">
+    <div className="w-full h-auto flex items-center ">
       {!isSearchFocused && <Sidebar />}
       {!isSearchFocused && (
         <img
           src={ActiveHeartImage}
           alt="Active Heart"
-          className="h-8 w-auto lg:hidden mr-4"
+          className="h-8 w-auto lg:hidden mr-28"
         />
       )}
-      <div className="lg:flex items-center"></div>
+      <div className="lg:flex mr-7 items-center"></div>
       <Searchbar onFocusChange={handleSearchFocus} />
       
     </div>
